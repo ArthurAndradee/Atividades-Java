0e19efa7be4b1de7318d5e8f77b147f7f11e3268
arthur andrade <arthurandrad.23@gmail.com>
2024-03-19 22:58:55 -0300

    Feat: Activity 7 and 8 finished

---
+++ Lista 4/Exercício 07/src/App.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner read = new Scanner(System.in);
+        int[] X = new int[10];
+        int[] Y = new int[10];
+        
+        for(int i = 0; i < X.length ; i++) {
+            System.out.println("insira o valor para os elementos do array: ");
+            X[i] = read.nextInt();
+
+            if (X[i]%2 == 0) {
+                Y[i] = X[i] * 2;
+            } else if (X[i]%2 != 0) {
+                Y[i] = X[i] * 3;
+            }
+        }
+
+        System.out.println("Vetor Y: " + Arrays.toString(Y));
+    }
+}
