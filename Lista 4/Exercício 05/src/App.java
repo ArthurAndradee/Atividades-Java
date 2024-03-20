ea9f0695c870e05fdd4533edad32cae1451f3c3d
arthur andrade <arthurandrad.23@gmail.com>
2024-03-19 22:08:56 -0300

    Feat: Activity 5 from list 4 finished

---
+++ Lista 4/Exercício 05/src/App.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner read = new Scanner(System.in);
+
+        int[] A = new int[10];
+        int[] B = new int[10];
+        int[] C = new int[10];
+
+        for (int i = 0; i < A.length; i++) {
+            System.out.println("Insira " + (i+1) + "o multiplicador");
+            A[i] = read.nextInt();
+            System.out.println("Insira " + (i+1) + "o multiplicado"); 
+            B[i] = read.nextInt();
+
+            C[i] = A[i] * B[i];
+        }
+
+
+        System.out.println("Resultado: " + Arrays.toString(C));
+    }
+}
