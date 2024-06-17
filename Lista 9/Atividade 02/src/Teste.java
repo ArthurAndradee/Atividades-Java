270ad4d709a3e109652d68827ed22c44d77a86ce
arthur andrade <arthurandrad.23@gmail.com>
2024-06-17 09:32:11 -0300

    Activity 02 from list 9 done

---
+++ Lista 9/Atividade 02/src/Teste.java
@@ -0,0 +1,9 @@
+public class Teste {
+    public static void main(String[] args) throws Exception {
+        Quadrado quadrado = new Quadrado(5.0);
+        System.out.println(quadrado);
+        
+        Retangulo retangulo = new Retangulo(4, 3);
+        System.out.println(retangulo);
+    }
+}
